0f5f32ac49d88b78829e9872a6ef20ba572de358
Julian Waller <git@julusian.co.uk>
2019-01-17 19:33:53 +0100

    chore(azure): Tidying

--- tools/windows/build.bat
+++ tools/windows/build.bat
@@ -29,34 +29,7 @@ msbuild "CasparCG Server.sln" /p:Configuration=RelWithDebInfo /m:%NUMBER_OF_PROC
 
 :: Create server folder to later zip
 set SERVER_FOLDER=casparcg_server
-if exist "%SERVER_FOLDER%" rmdir "%SERVER_FOLDER%" /s /q || goto :error
-mkdir "%SERVER_FOLDER%" || goto :error
-
-:: Copy deploy resources
-echo Copying deploy resources...
-xcopy ..\resources\windows\flash-template-host-files "%SERVER_FOLDER%" /E /I /Y || goto :error
-
-:: Copy binaries
-echo Copying binaries...
-copy shell\*.dll "%SERVER_FOLDER%" || goto :error
-copy shell\RelWithDebInfo\casparcg.exe "%SERVER_FOLDER%" || goto :error
-copy ..\src\shell\casparcg_auto_restart.bat "%SERVER_FOLDER%" || goto :error
-copy shell\casparcg.config "%SERVER_FOLDER%" || goto :error
-copy shell\*.ttf "%SERVER_FOLDER%" || goto :error
-copy shell\*.pak "%SERVER_FOLDER%" || goto :error
-copy shell\*.bin "%SERVER_FOLDER%" || goto :error
-copy shell\*.dat "%SERVER_FOLDER%" || goto :error
-xcopy packages\%CEF_VERSION%\CEF\locales "%SERVER_FOLDER%\locales" /E /I /Y || goto :error
-xcopy packages\%CEF_VERSION%\CEF\swiftshader "%SERVER_FOLDER%\swiftshader" /E /I /Y || goto :error
-
-del *_debug.dll || goto :error
-del *-d-2.dll || goto :error
-
-:: Copy documentation
-echo Copying documentation...
-copy ..\CHANGELOG.md "%SERVER_FOLDER%" || goto :error
-copy ..\LICENSE.md "%SERVER_FOLDER%" || goto :error
-copy ..\README.md "%SERVER_FOLDER%" || goto :error
+package.bat ..
 
 :: Create zip file
 echo Creating zip...
