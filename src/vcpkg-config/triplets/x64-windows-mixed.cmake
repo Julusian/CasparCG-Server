6c6df4fd54e5466141c6bf0b61fa49c280f92271
Julian Waller <git@julusian.co.uk>
2023-05-31 01:49:30 +0100

    wip: building close to before

--- src/vcpkg-config/triplets/x64-windows-mixed.cmake
+++ src/vcpkg-config/triplets/x64-windows-mixed.cmake
@@ -5,10 +5,13 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 
 set(VCPKG_CRT_LINKAGE dynamic)
 
-if(${PORT} MATCHES "boost-|lua-lsqlite3")
-    # set(VCPKG_CRT_LINKAGE static)
-    set(VCPKG_LIBRARY_LINKAGE static)
-else()
-    # set(VCPKG_CRT_LINKAGE dynamic)
+if(${PORT} MATCHES "libopenmpt|tesseract")
+    # this wants to be static, but there is a bug in the build process https://github.com/microsoft/vcpkg/issues/30030
     set(VCPKG_LIBRARY_LINKAGE dynamic)
+elseif(${PORT} MATCHES "tbb|sfml|openal|glew|Freeimage|ffmpeg")
+    # dynamic link specific dependencies
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+else()
+    # static link by default
+    set(VCPKG_LIBRARY_LINKAGE static)
 endif()
